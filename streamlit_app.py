37eeb426c0443c833e3c8451f49a948faa2419ea
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-12 13:26:53 +1000

    Update streamlit_app.py
    fixed error, deleted a line

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-#let users choose their own fruits
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
-
 #make a pick list so users can see what they've selected
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
