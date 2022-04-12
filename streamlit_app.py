b86d6922d69adc83ca7e366e398b04085de26cd4
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-12 13:15:59 +1000

    Update streamlit_app.py
    added multiselect so users can pick fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#let users choose their own fruits
+streamlit.multiselect("Pick some fruits:" list(my_fruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
