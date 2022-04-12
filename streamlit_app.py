c90db51937009ad0481f253be8b502c09680d14e
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-12 13:09:33 +1000

    Update streamlit_app.py
    Changed title from parents to mom's. 
    Added emojis to breakfast favourites
    added new header for breakfast smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
-streamlit.title('My Parent\'s Healthy New Diner')
-streamlit.header('Breakfast')
-streamlit.text('Apples')
-streamlit.text('Toast')
-streamlit.text('Oatmeal')
+
+streamlit.title('My Mom\'s Healthy New Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
