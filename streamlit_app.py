077f9e6febf03706876e405128d4bb3f7cacfaa9
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-12 13:12:32 +1000

    Update streamlit_app.py
    imported pandas, reading csv from s3 bucket for fruit list for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Mom\'s Healthy New Diner')
 
@@ -8,4 +10,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
