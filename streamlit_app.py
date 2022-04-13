971f0f6000e9477248ed4562832e1369c86cc647
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-14 09:50:12 +1000

    missing bracket

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.header("The fruit load list contains: ")
 streamlit.dataframe(my_data_rows)
 
 #allow user to add a fruit to the list
-add_my_fruit = streamlit.text_input("What fruit would you like to add?", "jackfruit"
+add_my_fruit = streamlit.text_input("What fruit would you like to add?", "jackfruit")
 streamlit.text("Thanks for adding " + add_my_fruit)
 
 
