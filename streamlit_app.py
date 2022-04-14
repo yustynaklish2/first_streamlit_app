84660923f7976c04ce10f89b9e107fdf2860264b
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-14 10:12:03 +1000

    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fixefruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized) 
     
