1b32a689f34d5e217ff443e089896c121a866659
Yustyna <103472878+yustynaklish2@users.noreply.github.com>
2022-04-14 10:09:47 +1000

    fixed indentation line 31

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ try:
   fruit_choice = streamlit.text_input("What fruit would you like information about?")
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
-   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  else:
+    fixefruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized) 
     
